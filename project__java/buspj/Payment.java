c54cdba0e469082f503d8868ec911d8f03681859
gilgagun <ksm2853305@gmail.com>
2021-11-22 00:55:30 +0900

    페이먼트

--- project__java/buspj/Payment.java
+++ project__java/buspj/Payment.java
@@ -21,12 +21,30 @@ public class Payment extends JFrame {
         top.setBackground(Color.LIGHT_GRAY);
         top.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 16));
 
+
         // 서브 타이틀 문구
         JLabel title = new JLabel("마법의 성이라는 노래를 아십니까? 안다면 당신은 옛날 사람.");
         title.setFont(new Font("Serif", Font.BOLD, 20));
         top.add(title);
 
-        // 
+        // 뒤로가기 버튼
+        JPanel back = new JPanel();
+        setBackground(Color.LIGHT_GRAY);
+        this.frame = frame;
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 5));
+
+        JButton back = new JButton("뒤로가기");
+        back.setPreferredSize(new Dimension(120, 50));
+        back.setFont(new Font("고딕", Font.BOLD, 20));
+        add(back);
+
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                new Main(id);
+                frame.dispose();
+            }
+        });
 
         addWindowListener(new JFrameWindowClosingEventHandler());
         setVisible(true);
